94fc154c934f5dc29ce61cfa552ac12f4e0d1fa6
Roman <sporco1988@gmail.com>
2022-02-06 21:57:47 +0700

    added tuple print

--- print_ip.h
+++ print_ip.h
@@ -47,3 +47,51 @@ print_ip(const Container<T, Allocator>& cont) {
 	}
 	std::cout << std::endl;
 }
+
+//WORK WITH TUPLE
+
+//Checking tuple has all alements are the same type (badly, because isn't working with less then two elements)
+template<typename Prev, typename Cur, typename... Rest>
+struct is_tuple_same {
+	static const bool value = std::is_same_v<Prev, Cur> && is_tuple_same<Cur, Rest...>::value;
+};
+
+template<typename Prev, typename Cur>
+struct is_tuple_same<Prev, Cur> {
+	static const bool value = std::is_same_v<Prev, Cur>;
+};
+
+//print tuple
+template <int Index = 0, typename... Args>
+typename std::enable_if<
+	Index >= sizeof... (Args)
+	, void>::type
+	print_tuple(const std::tuple<Args...>& tup) {}
+
+template <int Index = 0, typename... Args>
+typename std::enable_if<
+	Index == (sizeof... (Args) - 1)
+	, void>::type
+	print_tuple(const std::tuple<Args...>& tup) {
+	std::cout << std::get<Index>(tup) << std::endl;
+}
+
+template <int Index = 0, typename... Args>
+typename std::enable_if<
+	Index < (sizeof... (Args) - 1)
+	, void>::type
+	print_tuple(const std::tuple<Args...>& tup) {
+	std::cout << std::get<Index>(tup) << ".";
+	print_tuple<Index + 1, Args...>(tup);
+}
+
+template< template <typename...> typename Tuple
+	, typename... Args
+>
+typename std::enable_if<
+	std::is_same<Tuple<Args...>, std::tuple<Args...>>::value &&
+	is_tuple_same<Args...>::value
+	, void>::type
+print_ip(const Tuple<Args...>& tup) {
+	print_tuple<0, Args...>(tup);
+}
