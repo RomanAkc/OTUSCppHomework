2fa7b30444aa55eb7046157268d14f24ebcac30b
roman <sporco1988@gmail.com>
2022-04-03 08:40:07 +0700

    deleted comment

--- main.cpp
+++ main.cpp
@@ -23,19 +23,7 @@ int main(int argc, char ** argv) {
     //TODO: compare
 
     //TODO: print result
-
-    /*auto vecDir= vm["dir"].as<std::vector<std::string>>();
-    for(auto& dir : vecDir) {
-        std::cout << dir << std::endl;
-    }
-
-    if(vm["level"].as<int>() == 1 || vm["level"].as<int>() == 0) {
-        std::cout << "level = " << vm["level"].as<int>() << std::endl;
-    }
-    else {
-        std::cout << "Bad command line. Use --help for more information" << std::endl;
-    }*/
-
+ 
 	std::cout << "Hello, world!" << std::endl;
 	return 0;
 }
